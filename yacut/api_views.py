a680c19aed519ccd7a9cf6130483fc1e32c8d362
Егор Житников <zhitnikoveg@yandex.ru>
2023-07-27 18:16:53 +0300

    first

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -19,17 +19,17 @@ def create_url():
     if 'url' not in data:
         raise InvalidAPIUsage('"url" является обязательным полем!')
     if URLMap.query.filter_by(short=data['custom_id']).first() is not None:
-        raise InvalidAPIUsage(f'Имя {data["custom_id"]} уже занято.')
+        raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
     url_map = URLMap(original=data.get('url'), short=data['custom_id'])
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify({url_map.to_dict()}), 201
+    return jsonify(url_map.to_dict()), HTTPStatus.CREATED.value
 
 
 @app.route('/api/id/<short_id>/', methods=['GET'])
 def get_original_url(short_id):
     urlmap = URLMap.query.filter_by(short=short_id).first()
     if urlmap is None:
-        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
-    return jsonify(url=urlmap.original), HTTPStatus.OK.value
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND.value)
+    return jsonify({'url': urlmap.to_dict()['url']}), HTTPStatus.OK.value
